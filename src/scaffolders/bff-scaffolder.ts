2a5635e7b022f6a9839c33044317556ea3f023ba
bgyyo622-Boduen <bgyyo622@photographybyai.net>
2026-05-19 20:27:21 +0800

    Update bff-scaffolder.ts

--- src/scaffolders/bff-scaffolder.ts
+++ src/scaffolders/bff-scaffolder.ts
@@ -2,6 +2,7 @@ import * as path from "path";
 import { RigidTemplater } from "../core/rigid-templater";
 
 export class BffScaffolder {
+  // 嚴格檢視：所需依賴皆透過介面顯式宣告，確保模組的獨立性與可測試性
   constructor(
     private projectName: string,
     private outputDir: string,
@@ -14,6 +15,7 @@ export class BffScaffolder {
     const bffTemplateDir = path.join(this.templateDir, "bff");
     const bffOutputDir = path.join(this.outputDir, "bff/src/middlewares");
 
+    // 嚴格檢視：呼叫決定性渲染器，注入的 Context 為空物件 {}，因為底層外殼不容許動態竄改
     RigidTemplater.renderAndWrite(
       path.join(bffTemplateDir, "middleware.ts.hbs"),
       path.join(bffOutputDir, "auth-middleware.ts"),
